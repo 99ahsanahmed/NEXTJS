d9d3c377832c9b23c8a6925ad8b17b7e6c163329
Ahsan Siddiqui <ahsan.ha58@gmail.com>
2024-09-12 16:42:33 +0500

    add about file to test

---
+++ 02_linking/src/app/about/page.tsx
@@ -0,0 +1,10 @@
+const about = () => {
+  return (
+    <div>Hello This is an about page but 
+        <h1>not inside master branch</h1>
+        <h2>Instead inside a test branch</h2>
+    </div>
+  )
+}
+
+export default about
