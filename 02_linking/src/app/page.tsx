e85136f870d5f15bf597527a361bd1a3b72ca782
Ahsan Siddiqui <ahsan.ha58@gmail.com>
2024-09-12 16:15:09 +0500

    page added

--- 02_linking/src/app/page.tsx
+++ 02_linking/src/app/page.tsx
@@ -4,6 +4,11 @@ export default function Home() {
   return (
     <>
     <h1>Hello World</h1>
+    <ul>
+      <li>Home</li>
+      <li>About</li>
+      <li>Contact US</li>
+    </ul>
     </>
   );
 }
