fbb1765302a5860f3a8087ad1c61d15dccfd9b20
Ahsan Siddiqui <ahsan.ha58@gmail.com>
2024-09-13 19:49:28 +0500

    add portfolio navbar

--- 02_Portfolio/src/app/page.tsx
+++ 02_Portfolio/src/app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
-
+import Navbar from "@/components/Navbar"
 export default function Home() {
   return (
-    <>
-    <h1>Hello World</h1>
-    </>
+    <div className="main   " >
+    <Navbar/>
+    <h1 className="bg-gray-700" >Hello World abra ka dabra <br/> hawuehduasf <br/> yasbcb</h1>
+    </div>
   );
 }
--- 02_Portfolio/src/components/Navbar.tsx
+++ 02_Portfolio/src/components/Navbar.tsx
@@ -1,8 +1,14 @@
+import Link from "next/link"
 const Navbar = () => {
   return (
-    <>
-    
-    </>
+    <div className="navbar sticky top-0 flex items-center bg-white/30 backdrop-blur-sm  text-black font-bold text-lg uppercase">
+        <h1 className="text-3xl font-serif" >Logo</h1>
+        <ul className="flex gap-4 absolute right-0 px-8 " >
+            <Link href="/" ><li>Home</li> </Link>
+            <li>About</li>
+            <li>Connect</li>
+        </ul>
+     </div>
   )
 }
 
