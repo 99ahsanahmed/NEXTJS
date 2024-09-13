c5e27e5301c70be5ddacfa8e4f4bb867b38ea800
Ahsan Siddiqui <ahsan.ha58@gmail.com>
2024-09-13 14:40:02 +0500

    learning stash

--- 02_linking/src/app/about/page.tsx
+++ 02_linking/src/app/about/page.tsx
@@ -3,6 +3,7 @@ const about = () => {
     <div>Hello This is an about page but 
         <h1>not inside master branch</h1>
         <h2>Instead inside a test branch</h2>
+        <h4>Bhai mujhe stash sikaho</h4>
     </div>
   )
 }
